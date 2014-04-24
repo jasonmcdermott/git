cf631cec25e896c3e3328b39363c91bc2761a9e1
Jason Macbook Air <jason@jasonmcdermott.net>
2014-04-25 08:46:09 +1000

    two new files

---
+++ gitTrial/SENTrialViewController.h
@@ -0,0 +1,13 @@
+//
+//  SENTrialViewController.h
+//  gitTrial
+//
+//  Created by Jason McDermott on 25/04/2014.
+//  Copyright (c) 2014 Sensorium Health. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SENTrialViewController : UIViewController
+
+@end
---
+++ gitTrial/SENTrialViewController.m
@@ -0,0 +1,49 @@
+//
+//  SENTrialViewController.m
+//  gitTrial
+//
+//  Created by Jason McDermott on 25/04/2014.
+//  Copyright (c) 2014 Sensorium Health. All rights reserved.
+//
+
+#import "SENTrialViewController.h"
+
+@interface SENTrialViewController ()
+
+@end
+
+@implementation SENTrialViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
