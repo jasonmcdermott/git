1432fa5e1b0db3bf153b8e7f2c31aaeb96b62aa5
Jason Macbook Air <jason@jasonmcdermott.net>
2014-04-25 08:36:09 +1000

    Initial Commit

---
+++ gitTrial/SENAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SENAppDelegate.h
+//  gitTrial
+//
+//  Created by Jason McDermott on 25/04/2014.
+//  Copyright (c) 2014 Sensorium Health. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SENAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
